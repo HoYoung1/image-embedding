93d5b5bd5331f89e20542ae21a311657b74190f6
aeg <aeg@amazon.com>
2019-07-11 16:05:06 -0700

    Refactor

--- src/resnetembedding.py
+++ src/resnetembedding.py
@@ -6,23 +6,35 @@ from torchvision.transforms import transforms
 
 class ImageEmbedder:
 
-    def __init__(self):
+    def __init__(self, min_img_size_h=214, min_img_size_w=214, device=None, ):
+        self.min_img_size_h = min_img_size_h
+        self.min_img_size_w = min_img_size_w
 
-        self.model =  models.resnet18(pretrained=True)
+        # Set up default device
+        self.device = device or ('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.model = models.resnet18(pretrained=True)
+        self.model.to(device=self.device)
 
     def __call__(self, image):
         self.model.eval()
-        # torch image: C X H X W
-        with torch.no_grad():
-            image = Image.fromarray(image)
-            min_img_size = 214  # The min size, as noted in the PyTorch pretrained models doc, is 224 px.
-            transform_pipeline = transforms.Compose([transforms.Resize((min_img_size, min_img_size)),
-                                                     transforms.ToTensor(),
-                                                     transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                                          std=[0.229, 0.224, 0.225])])
-            img = transform_pipeline(image)
 
-            img_tensor = img.unsqueeze(0)
+        with torch.no_grad():
+            img_tensor = self.pre_process_image(image)
+            img_tensor.to(device=self.device)
             result = self.model(img_tensor)
 
         return result
+
+    def pre_process_image(self, image):
+        # pre-process data
+        image = Image.fromarray(image)
+        # The min size, as noted in the PyTorch pretrained models doc, is 224 px.
+        transform_pipeline = transforms.Compose([transforms.Resize((self.min_img_size_h, self.min_img_size_w)),
+                                                 transforms.ToTensor(),
+                                                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                                      # torch image: C X H X W
+                                                                      std=[0.229, 0.224, 0.225])])
+        img = transform_pipeline(image)
+        # Add batch [N, C, H, W]
+        img_tensor = img.unsqueeze(0)
+        return img_tensor
