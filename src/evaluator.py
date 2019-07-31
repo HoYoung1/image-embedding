2c8d660e9302af45579b2a1b6317889a86bf54f3
aeg <aeg@amazon.com>
2019-07-31 21:38:36 +1000

    try optmise mem usage

--- src/evaluator.py
+++ src/evaluator.py
@@ -23,10 +23,6 @@ class Evaluator:
         embeddings = torch.stack(embeddings)
         class_person = torch.stack(class_person)
 
-        # clean up gpu memory
-        del self.model
-        torch.cuda.empty_cache()
-
         # Compute pairwise
         pairwise_distance = self.distance_metric(embeddings)
 
