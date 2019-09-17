af9f29506329ac32e972c0ba97e5b7f177329d91
aeg <aeg@amazon.com>
2019-09-17 15:33:03 +1000

    Add dropout

--- src/ModelResnet.py
+++ src/ModelResnet.py
@@ -4,15 +4,16 @@ from torchvision import models
 
 class ModelResnet(nn.Module):
 
-    def __init__(self, n_classes):
+    def __init__(self, n_classes, drop_out=0.5):
         super().__init__()
+        self.drop_out = drop_out
         self.model = models.resnet50(pretrained=True)
         self.classes = n_classes
         # Change the final layer so that the number of classes
         # Use print final layer to figure out the input size to the final layer
         # print(self.model.fc)
         fc_input_size = 2048
-        self.model.fc = nn.Linear(fc_input_size, self.classes)
+        self.model.fc = nn.Sequential(nn.Dropout(p=self.drop_out), nn.Linear(fc_input_size, self.classes))
 
     def forward(self, input):
         fc_out = self.model(input)
