f8638e35c676c070ea15e5d745cbe6a447822db9
aeg <aeg@amazon.com>
2019-09-27 20:30:44 +1000

    Add license

--- src/cmc_score.py
+++ src/cmc_score.py
@@ -1,17 +1,31 @@
+# *****************************************************************************
+# * Copyright 2019 Amazon.com, Inc. and its affiliates. All Rights Reserved.  *
+#                                                                             *
+# Licensed under the Amazon Software License (the "License").                 *
+#  You may not use this file except in compliance with the License.           *
+# A copy of the License is located at                                         *
+#                                                                             *
+#  http://aws.amazon.com/asl/                                                 *
+#                                                                             *
+#  or in the "license" file accompanying this file. This file is distributed  *
+#  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either  *
+#  express or implied. See the License for the specific language governing    *
+#  permissions and limitations under the License.                             *
+# *****************************************************************************
+
 import numpy as np
 import torch
 
-"""
-Definition: https://www.nist.gov/sites/default/files/documents/2016/12/06/12_ross_cmc-roc_ibpc2016.pdf
-- Each sample is compared against all gallery samples. The resulting scores are sorted and ranked
-- Determine the rank at which a true match occurs
-- True Positive Identification Rate (TPIR): Probability of observing the correct identity within the top K ranks
-- CMC Curve: Plots TPIR against ranks
-- CMC Curve: Rank based metric
-"""
-
 
 class CMCScore:
+    """
+    Definition: https://www.nist.gov/sites/default/files/documents/2016/12/06/12_ross_cmc-roc_ibpc2016.pdf
+    - Each sample is compared against all gallery samples. The resulting scores are sorted and ranked
+    - Determine the rank at which a true match occurs
+    - True Positive Identification Rate (TPIR): Probability of observing the correct identity within the top K ranks
+    - CMC Curve: Plots TPIR against ranks
+    - CMC Curve: Rank based metric
+    """
 
     def accuracy_at_top_k(self, pairwise_distance_matrix, target_label, k):
         target_label = target_label.cpu().numpy()
