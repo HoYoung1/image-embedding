103bbca5ac2cbe61393ccd141adfb02f7f402a5f
aeg <aeg@amazon.com>
2019-09-30 14:02:52 +1000

    Change aspect ratio

--- src/datasets/market1501_factory.py
+++ src/datasets/market1501_factory.py
@@ -8,6 +8,7 @@ class Market1501Factory(CustomDatasetFactoryBase):
         pass
 
     def get(self, images_dir):
-        dataset = Market1501Dataset(images_dir)
+        # Market150 dataset size is 64 width, height is 128, so we maintain the aspect ratio
+        dataset = Market1501Dataset(images_dir, min_img_size_h=256, min_img_size_w=128)
 
         return dataset
